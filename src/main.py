90dd8c0a76be46922cb2a7b123fb2621e2f699de
shiftleino <>
2022-07-26 22:27:57 +0300

    Start doing main game loop

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from ui.console import ConsoleUI
+from entities.board import Board
+from entities.game import Game
 
 def main():
-    ui = ConsoleUI()
-    ui.create_start_menu()
+    board = Board()
+    game = Game(board)
+    game.start_game()
+    ui = ConsoleUI(board)
+    heuristic = ui.create_start_menu()
+    while heuristic not in ("1", "2"):
+        print("Invalid choice. Restarting game...")
+        game.start_game()
+        heuristic = ui.create_start_menu()
+    game.set_heuristic(heuristic)
+    ui.print_board()
+    restart_choice = ui.create_end_menu()
+
+    while restart_choice == "s":
+        heuristic = ui.create_start_menu()
+        while heuristic not in ("1", "2"):
+            print("Invalid choice. Restarting game...")
+            heuristic = ui.create_start_menu()
+        ui.print_board()
+        restart_choice = ui.create_end_menu()
+
 
 if __name__ == "__main__":
     main()
