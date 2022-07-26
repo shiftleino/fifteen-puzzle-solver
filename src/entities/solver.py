1acf314935f873986246c0d9ad9ae512c4875099
shiftleino <>
2022-07-26 18:23:20 +0300

    Add doc to Solver-class

--- src/entities/solver.py
+++ src/entities/solver.py
@@ -1,3 +1,10 @@
 class Solver:
-    def __init__(self):
+    """The Solver-class is responsible for the solving of the Fifteen Puzzle
+    using the IDA*-algorithm with two different heuristics: Manhattan-distance
+    and Hamming-distance.
+    """
+    def __init__(self, heuristic):
+        self.heuristic = heuristic
+
+    def solve_puzzle(self):
         pass
