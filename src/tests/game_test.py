eb30f46aafce63be307c8c333be076a2beab2842
shiftleino <>
2022-07-30 14:45:32 +0300

    Add tests for the Game-class

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -6,6 +6,7 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.board = Board()
         self.game = Game(self.board)
+        self.correct = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
     def test_set_heuristic_manhattan(self):
         heuristic = "1"
@@ -35,16 +36,22 @@ class TestGame(unittest.TestCase):
         self.assertEqual(sum(all_values), 0)
 
     def test_check_solvable(self):
-        pass
+        solvable = self.game.check_if_solvable(self.correct)
+        self.assertEqual(solvable, True)
+        board_values2 = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+        solvable2 = self.game.check_if_solvable(board_values2)
+        self.assertEqual(solvable2, True)
 
     def test_check_not_solvable(self):
         board_values = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 15, 14, 16]]
         solvable = self.game.check_if_solvable(board_values)
         self.assertEqual(solvable, False)
+        board_values2 = [[15, 14, 13, 12], [11, 10, 9, 8], [7, 6, 5, 4], [3, 2, 1, 16]]
+        solvable2 = self.game.check_if_solvable(board_values2)
+        self.assertEqual(solvable2, False)
 
     def test_check_correct(self):
-        board_values = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-        self.board.fill_board(board_values)
+        self.board.fill_board(self.correct)
         correct = self.game.check_if_correct()
         self.assertEqual(correct, True)
 
@@ -53,3 +60,12 @@ class TestGame(unittest.TestCase):
         self.board.fill_board(board_values)
         correct = self.game.check_if_correct()
         self.assertEqual(correct, False)
+
+    def test_blank_distance_same(self):
+        distance = self.game.get_blank_distance(self.correct)
+        self.assertEqual(distance, 0)
+
+    def test_blank_distance(self):
+        board_values = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+        distance = self.game.get_blank_distance(board_values)
+        self.assertEqual(distance, 3)
