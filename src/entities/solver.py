6d51966f129393c599477c77fa2ae8d93b45f431
shiftleino <>
2022-08-01 20:56:28 +0300

    Add manhattan distance func and test for it

--- src/entities/solver.py
+++ src/entities/solver.py
@@ -3,14 +3,34 @@ class Solver:
     using the IDA*-algorithm with two different heuristics: Manhattan-distance
     and Hamming-distance.
     """
-    def __init__(self, heuristic):
-        self.heuristic = heuristic
+    def __init__(self, game):
+        self.heuristic = game.heuristic
+        self.game = game
 
     def solve_puzzle(self):
         pass
 
-    def get_manhattan_distance(self):
-        pass
+    def get_manhattan_distance(self, tile_values):
+        """Calculates the Manhattan-distance of the given board to the correct solution.
+
+        Args:
+            tile_values ([][]int): The tile values of the current board.
+
+        Returns:
+            int: The Manhattan-distance.
+        """
+        total_distance = 0
+        for i in range(4):
+            for j in range(4):
+                value = tile_values[i][j]
+                correct_row = value // 4
+                correct_col = value % 4 - 1
+                if value % 4 == 0:
+                    correct_row -= 1
+                    correct_col = 3
+                sub_distance = abs(correct_row - i) + abs(correct_col - j)
+                total_distance += sub_distance
+        return total_distance
 
     def get_hamming_distance(self):
         pass
--- src/tests/solver_test.py
+++ src/tests/solver_test.py
@@ -1,7 +1,20 @@
 import unittest
 from entities.solver import Solver
+from entities.game import Game
+from entities.board import Board
 
 class TestSolver(unittest.TestCase):
     def setUp(self):
-        self.solver_manhattan = Solver("manhattan")
-        self.solver_hamming = Solver("hamming")
+        self.board = Board()
+        self.game = Game(self.board)
+        self.solver = Solver(self.game)
+        self.correct = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+    def test_manhattan_distance_correct(self):
+        distance = self.solver.get_manhattan_distance(self.correct)
+        self.assertEqual(distance, 0)
+
+    def test_manhattan_distance(self):
+        board_values = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+        distance = self.solver.get_manhattan_distance(board_values)
+        self.assertEqual(distance, 32)
