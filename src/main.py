4a93939eea9e3ba5145348898223c11abbbba015
shiftleino <>
2022-08-07 15:38:30 +0300

    Add solver class to the main loop

--- src/main.py
+++ src/main.py
@@ -1,28 +1,36 @@
 from ui.console import ConsoleUI
-from entities.board import Board
 from entities.game import Game
 
+
 def main():
-    board = Board()
-    game = Game(board)
-    game.start_game()
-    ui = ConsoleUI(board)
-    heuristic = ui.create_start_menu()
+    game = Game()
+    ui = ConsoleUI()
+    start_tile_values = game.start_game()
+
+    heuristic = ui.print_start_menu()
     while heuristic not in ("1", "2"):
         print("Invalid choice. Restarting game...")
-        game.start_game()
-        heuristic = ui.create_start_menu()
-    game.set_heuristic(heuristic)
-    ui.print_board()
-    restart_choice = ui.create_end_menu()
+        start_tile_values = game.start_game()
+        heuristic = ui.print_start_menu()
 
+    game.set_heuristic(heuristic)
+    ui.print_start_position(start_tile_values)
+    solution_steps, duration = game.solve_puzzle()
+    ui.print_solution_steps(solution_steps, duration)
+    
+    restart_choice = ui.print_end_menu()
     while restart_choice == "s":
-        heuristic = ui.create_start_menu()
+        heuristic = ui.print_start_menu()
         while heuristic not in ("1", "2"):
             print("Invalid choice. Restarting game...")
-            heuristic = ui.create_start_menu()
-        ui.print_board()
-        restart_choice = ui.create_end_menu()
+            start_tile_values = game.start_game()
+            heuristic = ui.print_start_menu()
+
+        game.set_heuristic(heuristic)
+        ui.print_start_position(start_tile_values)
+        solution_steps, duration = game.solve_puzzle()
+        ui.print_solution_steps(solution_steps, duration)
+        restart_choice = ui.print_end_menu()
 
 
 if __name__ == "__main__":
