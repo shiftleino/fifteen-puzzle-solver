945e7196fe6ad806f8c9a52a413e1b4231368be7
shiftleino <>
2022-08-12 23:14:58 +0300

    Modify UI struct

--- src/ui/console.py
+++ src/ui/console.py
@@ -5,14 +5,23 @@ class ConsoleUI:
 
     def print_start_menu(self):
         """Creates the start menu of the program and asks user's 
-        choice for the heuristic.
+        choice for the heuristic and the game mode (easy or hard).
 
         Returns:
             string: User's choice for the heuristic: "1" or "2".
+            string: User's choice for the mode: "1" or "2".
         """
         print("Fifteen Puzzle Solver\n")
-        choice = input("Choose which heuristic to use:\n1. Manhattan-distance\n2. Hamming-distance\n3. Improved Manhattan-distance\n>>> ")
-        return choice
+        heuristic = input("Choose which heuristic to use:\n1. Manhattan-distance\n2. Hamming-distance\n3. Improved Manhattan-distance\n>>> ")
+        while heuristic not in ("1", "2", "3"):
+            print("Invalid choice. Try again.")
+            heuristic = input("Choose which heuristic to use:\n1. Manhattan-distance\n2. Hamming-distance\n3. Improved Manhattan-distance\n>>> ")
+        
+        mode = input("Choose which mode to use:\n1. Easy\n2. Hard (NOTE: the algorithm might not find a solution in reasonable time.)\n>>> ")
+        while mode not in ("1", "2"):
+            print("Invalid choice. Try again.")
+            mode = input("Choose which mode to use:\n1. Easy\n2. Hard (NOTE: the algorithm might not find a solution in reasonable time.)\n>>> ")
+        return heuristic, mode
 
     def print_end_menu(self):
         """Creates the end menu of the program and asks user's
@@ -37,10 +46,10 @@ class ConsoleUI:
         """Shows the optimal steps that the solver took to solve the puzzle.
 
         Args:
-            solution_steps (_type_): _description_
-            duration (_type_): _description_
+            solution_steps ([][][]int): List of the boards visited before the solution.
+            duration (float): The time it took to solve the puzzle.
         """
-        print(f"\nA solution to the puzzle was found in {duration:.2f} seconds, the optimal steps are:\n")
+        print(f"\nA solution to the puzzle was found in {duration:.5f} seconds, the optimal steps are:\n")
         for board in solution_steps:
             self.print_board(board)
 
