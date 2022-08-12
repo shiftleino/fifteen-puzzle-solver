c00b462643054ae6042fe1e453ae976a92dd6444
shiftleino <>
2022-08-12 23:15:24 +0300

    Modify main game loop structure

--- src/main.py
+++ src/main.py
@@ -2,35 +2,27 @@ from ui.console import ConsoleUI
 from entities.game import Game
 
 
-def main():
-    game = Game()
-    ui = ConsoleUI()
-    start_tile_values = game.start_game()
-
-    heuristic = ui.print_start_menu()
-    while heuristic not in ("1", "2", "3"):
-        print("Invalid choice. Restarting game...")
-        start_tile_values = game.start_game()
-        heuristic = ui.print_start_menu()
-
+def run_game(game, ui):
+    heuristic, mode = ui.print_start_menu()
     game.set_heuristic(heuristic)
+    if mode == "1":
+        start_tile_values = game.start_game_easy()
+    else:
+        start_tile_values = game.start_game_hard()
+
     ui.print_start_position(start_tile_values)
     solution_steps, duration = game.solve_puzzle()
     ui.print_solution_steps(solution_steps, duration)
-    
     restart_choice = ui.print_end_menu()
-    while restart_choice == "s":
-        heuristic = ui.print_start_menu()
-        while heuristic not in ("1", "2", "3"):
-            print("Invalid choice. Restarting game...")
-            start_tile_values = game.start_game()
-            heuristic = ui.print_start_menu()
+    return restart_choice
 
-        game.set_heuristic(heuristic)
-        ui.print_start_position(start_tile_values)
-        solution_steps, duration = game.solve_puzzle()
-        ui.print_solution_steps(solution_steps, duration)
-        restart_choice = ui.print_end_menu()
+def main():
+    game = Game()
+    ui = ConsoleUI()
+    restart_choice = "s"
+    
+    while restart_choice == "s":
+        restart_choice = run_game(game, ui)
 
 
 if __name__ == "__main__":
