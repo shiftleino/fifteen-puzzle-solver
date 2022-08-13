e0a4bb5ed91c5d48f18e75174f0e636f47e3a58e
shiftleino <>
2022-08-13 16:59:10 +0300

    Modify performance testing

--- src/performance_testing.py
+++ src/performance_testing.py
@@ -1,4 +1,4 @@
-import timeit
+import time
 from entities.solver import Solver
 from entities.game import Game
 
@@ -17,9 +17,16 @@ def performance_test_easy():
     print("Average durations:")
     heuristics = [("1", "Manhattan"), ("2", "Hamming"), ("3", "Improved Manhattan")]
     for num, heuristic in heuristics:
-        solver = set_up(num, "easy")
-        durations = timeit.repeat(str(solver.solve_puzzle()), number=100)
-        avg_duration = sum(durations) / 10
+        total_time = 0
+        for _ in range(100):
+            solver = set_up(num, "easy")
+            start = time.time()
+            solver.solve_puzzle()
+            end = time.time()
+            sub_time = end - start
+            total_time += sub_time
+
+        avg_duration = total_time / 100
         print(f"{heuristic}: {avg_duration:.10f}s")
     print()
 
@@ -27,9 +34,16 @@ def performance_test_intermediate():
     print("Average durations:")
     heuristics = [("1", "Manhattan"), ("2", "Hamming"), ("3", "Improved Manhattan")]
     for num, heuristic in heuristics:
-        solver = set_up(num, "intermediate")
-        durations = timeit.repeat(str(solver.solve_puzzle()), number=100)
-        avg_duration = sum(durations) / 10
+        total_time = 0
+        for _ in range(100):
+            solver = set_up(num, "intermediate")
+            start = time.time()
+            solver.solve_puzzle()
+            end = time.time()
+            sub_time = end - start
+            total_time += sub_time
+
+        avg_duration = total_time / 100
         print(f"{heuristic}: {avg_duration:.10f}s")
     print()
 
